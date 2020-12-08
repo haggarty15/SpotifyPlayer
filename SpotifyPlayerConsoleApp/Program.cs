a0bc3186d2e8c5ee4bb47d743380ca8d8debe57b
Kyle Haggarty <kylehaggarty@gmail.com>
2020-12-08 03:22:23 +0000

    adding console app project and library apiManager

---
+++ SpotifyPlayerConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using SpotifyApi.Web;
+
+namespace SpotifyPlayerConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+        }
+    }
+}
--- SpotifyPlayerLibrary/SpotifyPlayerLibrary/SpotifyApiManager.cs
+++ SpotifyPlayerLibrary/SpotifyPlayerLibrary/SpotifyApiManager.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SpotifyAPI;
 
 namespace SpotifyPlayerLibrary
 {
-    public class Class1
+    public class SpotifyApiManager
     {
+
+
+        public SpotifyApiManager()
+        {
+
+        }
     }
 }
